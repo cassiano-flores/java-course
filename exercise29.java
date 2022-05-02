b71b034e293992a6a3e149ff70a60857c98e0261
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-05-02 16:16:58 -0300

    testing bitwise

---
+++ exercise29.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercise29 {
+    
+    //testing bitwise
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int mask = 32;
+        int n = sc.nextInt();
+
+        if ((mask & n) != 0){
+            System.out.println("6th bit is true! ( = 1)");
+        }
+        else{
+            System.out.println("6th bit is false! ( = 0)");
+        }
+
+        sc.close();
+    }
+}
