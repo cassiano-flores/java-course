c91a454253453afb4feb213a6d3d097c45f0204e
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-05-17 11:06:00 -0300

    encapsulation

--- object_oriented_programming/entities/Product.java
+++ object_oriented_programming/entities/Product.java
@@ -3,18 +3,42 @@ package object_oriented_programming.entities;
 //class Product for Exercise37
 public class Product {
     
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
-    public Product(String name, double price, int quantity) {
+    public Product(String name, double price, int quantity) {    //constructor
 
         this.name = name;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public double totalValueInStock(){
+    public String getName() {                           //getters and setters
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    //public void setQuantity(int quantity) {       //not make sense a setQuantity in this business
+    //    this.quantity = quantity;
+    //}
+
+    public double totalValueInStock(){          //methods
         
         return price * quantity;
     }
@@ -29,7 +53,7 @@ public class Product {
         this.quantity = this.quantity - quantity;
     }
 
-    public String toString(){
+    public String toString(){              //toString (convert the results in a String)
 
         return name 
                 + ", $ " 
